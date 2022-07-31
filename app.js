ba1c0b5be1032c6e452efbd2c94a0c3908662e42
CoachemUp <jeffcobb1206@gmail.com>
2022-07-31 18:52:40 -0400

    set the display

--- app.js
+++ app.js
@@ -13,20 +13,68 @@ start.addEventListener("click", () => {
 });
 
 //Phrases for game
-const phrases = [
+let phrases = [
   "May the Force be with you",
   "Houston we have a problem",
   "I am your father",
   "Just keep swimming",
-  "Bond James Bond"
+  "Bond James Bond",
 ];
 
 function getRandomPhraseAsArray(arr) {
-  const phraseIndex = Math.floor(Math.random() * phrases.lenth);
+  const phraseIndex = Math.floor(Math.random() * phrases.length);
   const randomPhrase = phrases[phraseIndex];
-  const splitPhrase = randomPhrase.split('');
+  const splitPhrase = randomPhrase.split("");
   return splitPhrase;
 }
 
 const phraseArray = getRandomPhraseAsArray(phrases);
 
+console.log(phraseArray)
+
+
+
+
+//Set the game display
+function addPhraseToDisplay(arr) {
+  for (let i = 0; i < phraseArray.length; i++) {
+    const li = phraseArray[i];
+    const liPhrase = document.createElement("li");
+    liPhrase.textContent = li;
+    const ul = document.querySelector("#phrase ul");
+    ul.appendChild(liPhrase);
+
+    if (li !== " ") {
+      liPhrase.classList.add("letter");
+    } else if (li === " ") {
+      liPhrase.classList.add("space");
+    }
+  }
+}
+
+//Check letter function
+function checkLetter(guess) {
+  const correct = phraseArray.includes(guess);
+
+  if (correct === true) {
+    for (let i = 0; i < phraseArray.length; i++)
+      if (phraseArray[i] === guess) {
+        document.querySelectorAll("li")[i].classList.add("show");
+      }
+  } else {
+    const attempts = document.querySelectorAll("img");
+    attempts[missed].src = "images/lostHeart.png";
+    missed++;
+  }
+}
+
+//Event listener for Attempts
+qwerty.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON" && e.target.className != "chosen") {
+    const btn = e.target;
+    btn.disabled = true;
+    btn.className = "chosen";
+    const letterCheck = e.target.textContent;
+    const letterFound = checkLetter(letterCheck);
+  }
+});
